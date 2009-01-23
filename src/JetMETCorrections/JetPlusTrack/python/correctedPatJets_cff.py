cbdc677da24ceee34edf085ffad6fcfed5d06e5a
bainbrid <>
2009-01-23 13:58:28 +0000

    fixes to JetCorrFactorProducer module, added energy scale histos

--- src/JetMETCorrections/JetPlusTrack/python/correctedPatJets_cff.py
+++ src/JetMETCorrections/JetPlusTrack/python/correctedPatJets_cff.py
@@ -2,15 +2,22 @@ import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.L2L3Corrections_Summer08_cff import *
 from JetMETCorrections.Configuration.L7PartonCorrections_cff import *
+
 from JetMETCorrections.JetPlusTrack.L1L3Corrections_JPT_cff import *
+L3JetCorrectorIC5PAT = L3JetCorrectorIC5JPT.clone(
+    label = cms.string('L3AbsoluteJetCorrectorPAT'),
+    JetTrackCollectionAtVertex = cms.InputTag("iterativeCone5JetTracksAssociatorAtVertex"),
+    JetTrackCollectionAtCalo   = cms.InputTag("iterativeCone5JetTracksAssociatorAtCaloFace")
+    )
 
 from RecoJets.JetAssociationProducers.iterativeCone5JTA_cff import *
 
 from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
 patJetCorrFactors = jetCorrFactors.clone(
-    L1JetCorrector      = cms.string('L2RelativeJetCorrectorIC5ZSP'),
+    jetSource           = cms.InputTag("iterativeCone5CaloJets"),
+    L1JetCorrector      = cms.string('L1OffsetJetCorrectorZSP'),
     L2JetCorrector      = cms.string('none'),
-    L3JetCorrector      = cms.string('L3AbsoluteJetCorrectorIC5JPT'),
+    L3JetCorrector      = cms.string('L3AbsoluteJetCorrectorPAT'),
     L7udsJetCorrector   = cms.string('none'),
     L7gluonJetCorrector = cms.string('none'),
     L7cJetCorrector     = cms.string('none'),
@@ -20,7 +27,7 @@ patJetCorrFactors = jetCorrFactors.clone(
 from PhysicsTools.PatAlgos.producersLayer1.jetProducer_cfi import * 
 correctedPatJets = allLayer1Jets.clone(
     jetSource            = cms.InputTag("iterativeCone5CaloJets"),
-    addJetCorrFactors    = cms.bool(False),
+    addJetCorrFactors    = cms.bool(True),
     jetCorrFactorsSource = cms.InputTag("patJetCorrFactors"),
     embedCaloTowers      = cms.bool(False),
     addResolutions       = cms.bool(False),
@@ -53,4 +60,27 @@ keepCorrectedPatJets = cms.PSet(
                                            )
     )
 
+from JetMETCorrections.JetPlusTrack.EnergyScaleHistogrammer_cfi import * 
+energyScaleHistogrammer.RootFileName = 'PatJetEnergyScale.root'
+
+from JetMETCorrections.JetPlusTrack.EnergyScaleAnalyzer_cfi import * 
+
+rawPatHistos = energyScaleAnalyzer.clone(
+    GenObjectType  = cms.string('GenJet'),
+    RecoObjectType = cms.string('CaloJet'),
+    GenObjectTag   = cms.InputTag('iterativeCone5GenJets'),
+    RecoObjectTag  = cms.InputTag('iterativeCone5CaloJets')
+    )
+
+jptPatHistos = energyScaleAnalyzer.clone(
+    GenObjectType  = cms.string('GenJet'),
+    RecoObjectType = cms.string('PatJet'),
+    GenObjectTag   = cms.InputTag('iterativeCone5GenJets'),
+    RecoObjectTag  = cms.InputTag('correctedPatJets')
+    )
+
+correctedPatJetHistos = cms.Sequence(
+    rawPatHistos +
+    jptPatHistos
+    )
 
