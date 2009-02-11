743f5571c7c7c685e629a01f8df064fd39a27312
bainbrid <>
2009-02-11 14:26:14 +0000

    added public default constructor, associated methods, bug fix to dEta and dPhi

--- src/JetMETCorrections/JetPlusTrack/interface/LorentzVectorPair.h
+++ src/JetMETCorrections/JetPlusTrack/interface/LorentzVectorPair.h
@@ -9,17 +9,32 @@ class LorentzVectorPair {
 
   // ---------- Con(de)structors ----------
   
-  /// 4-vector of just gen object
-  explicit LorentzVectorPair( const HepLorentzVector& gen_object );
-
   /// 4-vectors of gen and reco object
   explicit LorentzVectorPair( const HepLorentzVector& gen_object,
 			      const HepLorentzVector& reco_object );
   
+  /// 4-vector of just gen object
+  explicit LorentzVectorPair( const HepLorentzVector& gen_object );
+  
+  /// Default constructor
+  LorentzVectorPair();
+
+  /// Copy constructor
+  LorentzVectorPair( const LorentzVectorPair& );
+
   /// Destructor
   ~LorentzVectorPair();
 
   // ---------- Setter methods ----------
+  
+  /// Add 4-vector for gen object 
+  void gen( const HepLorentzVector& gen_object );
+  
+  /// Add gen object 
+  void gen( const double& px,
+	    const double& py,
+	    const double& pz,
+	    const double& e );
   
   /// Add 4-vector for reco object 
   void reco( const HepLorentzVector& reco_object );
@@ -49,19 +64,17 @@ class LorentzVectorPair {
   
   /// DR between gen and reco objects
   double dR() const;
-
- private:
+  
+  /// DR between gen and reco objects
+  double deltaR() const;
   
   // ---------- Utility methods ----------
   
   static const double pi_;
-  double dEta( double, double ) const;
-  double dPhi( double, double ) const;
-  double dR( double, double ) const;
+  static double dEta( double, double );
+  static double dPhi( double, double );
+  static double dR( double, double );
   
-  /// Private default constructor
-  LorentzVectorPair();
-
  private:
   
   /// 4-vector of gen object
@@ -69,62 +82,95 @@ class LorentzVectorPair {
   
   /// 4-vector of reco object
   HepLorentzVector reco_;
-
-  /// Indicates if both gen and reco objects are defined
-  bool both_;
-
+  
+  /// Indicates if gen object is defined
+  bool g_;
+  
+  /// Indicates if reco object is defined
+  bool r_;
+  
 };
 
 // ---------- Inline methods ----------
 
+inline void LorentzVectorPair::gen( const HepLorentzVector& object ) { 
+  gen_ = object; 
+  g_ = true;
+}
+
+inline void LorentzVectorPair::gen( const double& px,
+				    const double& py,
+				    const double& pz,
+				    const double& e ) { 
+  gen_ = HepLorentzVector(px,py,pz,e); 
+  g_ = true;
+}
+
 inline void LorentzVectorPair::reco( const HepLorentzVector& object ) { 
   reco_ = object; 
-  both_ = true;
+  r_ = true;
 }
 
 inline void LorentzVectorPair::reco( const double& px,
-				 const double& py,
-				 const double& pz,
-				 const double& e ) { 
+				     const double& py,
+				     const double& pz,
+				     const double& e ) { 
   reco_ = HepLorentzVector(px,py,pz,e); 
-  both_ = true;
+  r_ = true;
 }
 
-inline HepLorentzVector LorentzVectorPair::gen() const { return gen_; }
-
-inline HepLorentzVector LorentzVectorPair::reco() const {
-  if ( both_ ) { return reco_; }
+inline HepLorentzVector LorentzVectorPair::gen() const { 
+  if ( g_ ) { return gen_; }
   else { return HepLorentzVector(); }
 }
 
-inline double LorentzVectorPair::dEta() const { 
-  if ( both_ ) { return dEta( reco_.eta(), gen_.eta() ); }
-  else { return 0.; }
+inline HepLorentzVector LorentzVectorPair::reco() const {
+  if ( r_ ) { return reco_; }
+  else { return HepLorentzVector(); }
 }
 
-inline bool LorentzVectorPair::both() const { return both_; }
+inline bool LorentzVectorPair::both() const { return g_ && r_; }
+
+inline double LorentzVectorPair::dEta() const { 
+  if ( both() ) { return dEta( reco_.eta(), gen_.eta() ); }
+  else { return -1.; }
+}
 
 inline double LorentzVectorPair::dPhi() const { 
-  if ( both_ ) { return dPhi( reco_.phi(), gen_.phi() ); }
-  else { return 0.; }
+  if ( both() ) { return dPhi( reco_.phi(), gen_.phi() ); }
+  else { return -1.; }
+}
+
+inline double LorentzVectorPair::deltaR() const { 
+  if ( both() ) { return gen_.deltaR(reco_); }
+  else { return -1.; }
 }
 
 inline double LorentzVectorPair::dR() const { 
-  if ( both_ ) { return dR( dEta(), dPhi() ); }
-  else { return 0.; }
+  if ( both() ) { return dR( dEta(), dPhi() ); }
+  else { return -1.; }
 }
 
-inline double LorentzVectorPair::dEta( double phi1, double phi2 ) const {
+inline double LorentzVectorPair::dEta( double eta1, double eta2 ) {
+  return fabs( eta1 - eta2 ); 
+}
+
+inline double LorentzVectorPair::dPhi( double phi1, double phi2 ) {
   double dphi = fabs( phi1 - phi2 );
   return ( dphi > pi_ ?  2. * pi_- dphi : dphi );
 }
 
-inline double LorentzVectorPair::dPhi( double eta1, double eta2 ) const {
-  return fabs( eta1 - eta2 ); 
-}
+/* inline double LorentzVectorPair::dEta( double phi1, double phi2 ) { */
+/*   double dphi = fabs( phi1 - phi2 ); */
+/*   return ( dphi > pi_ ?  2. * pi_- dphi : dphi ); */
+/* } */
 
-inline double LorentzVectorPair::dR( double deta, double dphi ) const {
-  return sqrt( deta* deta + dphi * dphi );
+/* inline double LorentzVectorPair::dPhi( double eta1, double eta2 ) { */
+/*   return fabs( eta1 - eta2 );  */
+/* } */
+
+inline double LorentzVectorPair::dR( double deta, double dphi ) {
+  return sqrt( deta * deta + dphi * dphi );
 }
 
 #endif // ObjectMETCorrections_ObjectPlusTrack_LorentzVectorPair_H
