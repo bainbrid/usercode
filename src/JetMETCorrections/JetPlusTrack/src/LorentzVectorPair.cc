a9c10b10c224ca649a1767531e124cc1a371bdac
bainbrid <>
2009-02-11 14:26:47 +0000

    added public default constructor, associated methods

--- src/JetMETCorrections/JetPlusTrack/src/LorentzVectorPair.cc
+++ src/JetMETCorrections/JetPlusTrack/src/LorentzVectorPair.cc
@@ -6,10 +6,12 @@ const double LorentzVectorPair::pi_ = 3.14159265359;
 
 // -----------------------------------------------------------------------------
 // 
-LorentzVectorPair::LorentzVectorPair() 
-  : gen_(),
-    reco_(),
-    both_(false)
+LorentzVectorPair::LorentzVectorPair( const HepLorentzVector& gen_object,
+				      const HepLorentzVector& reco_object ) 
+  : gen_( gen_object ),
+    reco_( reco_object ),
+    g_(true),
+    r_(true)
 {;}
 
 // -----------------------------------------------------------------------------
@@ -17,16 +19,26 @@ LorentzVectorPair::LorentzVectorPair()
 LorentzVectorPair::LorentzVectorPair( const HepLorentzVector& gen_object ) 
   : gen_( gen_object ),
     reco_(),
-    both_(false)
+    g_(true),
+    r_(false)
 {;}
 
 // -----------------------------------------------------------------------------
 // 
-LorentzVectorPair::LorentzVectorPair( const HepLorentzVector& gen_object,
-			      const HepLorentzVector& reco_object ) 
-  : gen_( gen_object ),
-    reco_( reco_object ),
-    both_(true)
+LorentzVectorPair::LorentzVectorPair() 
+  : gen_(),
+    reco_(),
+    g_(false),
+    r_(false)
+{;}
+
+// -----------------------------------------------------------------------------
+// 
+LorentzVectorPair::LorentzVectorPair( const LorentzVectorPair& input ) 
+  : gen_( input.gen_ ),
+    reco_( input.reco_ ),
+    g_( input.g_ ),
+    r_( input.r_ )
 {;}
 
 // -----------------------------------------------------------------------------
