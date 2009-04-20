873e5d12f3c968bf8ccaa925ed54fce5d7c92112
bainbrid <>
2009-04-20 13:02:07 +0000

    config that picks up default config of photon id producer in reco chain

---
+++ Test/python/patPhotonIDProducer_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoEgamma.PhotonIdentification.photonId_cfi import PhotonIDProd
+
+patPhotonIDProducer = cms.EDProducer(
+    "PATPhotonIDProducer",
+    PhotonTag = cms.InputTag("selectedLayer1Photons"),
+    )
+
+for i in dir(PhotonIDProd):
+    if isinstance(getattr(PhotonIDProd,i),cms._ParameterTypeBase):
+        setattr(patPhotonIDProducer,i,getattr(PhotonIDProd,i))
