900c98830112c40521518412ad3788d3cb181c44
bainbrid <>
2009-06-11 08:31:59 +0000

    change name

--- Test/python/simplePhotonIDAnalysis_cfi.py
+++ Test/python/simplePhotonIDAnalysis_cfi.py
@@ -2,7 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 simplePhotonIDAnalysis = cms.EDFilter(
     "SimplePhotonIDAnalysis",
-    Photons      = cms.untracked.InputTag("patPhotonIDProducer"),
-    OtherPhotons = cms.untracked.InputTag("selectedLayer1Photons"),
+    Photons      = cms.InputTag("patPhotonIDProducer"),
+    OtherPhotons = cms.InputTag("selectedLayer1Photons"),
+    GenParticles = cms.InputTag("genParticles"),
+    PhotonsPdgIds = cms.vint32(),
+    LeptonsPdgIds = cms.vint32(),
+    ParticlesPdgIds = cms.vint32(),
     )
-
