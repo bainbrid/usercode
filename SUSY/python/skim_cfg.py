cef16002a021a3e9664795bf59fcf00cc3d95709
bainbrid <>
2010-08-24 16:32:03 +0000

    fix to prevent crash in skimmer

--- SUSY/python/skim_cfg.py
+++ SUSY/python/skim_cfg.py
@@ -39,24 +39,15 @@ skim_ps = PSet(
 # -----------------------------------------------------------------------------
 # Samples
 
-qcd=PSet(
-    Name="QCDPythia6",
-    File=["/vols/cms02/bm409/QCD_Pythia6_*GeV.root"],
-    Weights = PSet(
-    CrossSection = [ 8.762e+08, 6.041e+07, 9.238e+05, 2.547e+04, 1.256e+03, 8.798e+01, 2.186e+00, 1.122e-02 ],
-    Events       = [ 6246300,   5228992,   3203440,   3132800,   3274202,   2143390,   2143921,   1184123   ],
-    PtBin        = [ 15.,       30.,       80.,       170.,      300.,      470.,      800.,      1400.     ],
-    ),
-    Format=("ICF",2),
-    )
-#File=["results/prescale100/Skim_QCDPythia6_Pt*.root"]
+from samples_cff import *
 
 # -----------------------------------------------------------------------------
 # Definition of analyses
 
-anal = Analysis("QCDPythia6")
-addCutFlow(anal)
-skim = SkimOp( skim_ps.ps() )
-anal += skim
-anal.Run("../results",conf,[qcd])
+for bin in range(0,len(qcd6)) :
+    anal = Analysis("Empty")
+    addCutFlow(anal)
+    skim = SkimOp( skim_ps.ps() )
+    anal += skim
+    anal.Run("results",conf,[qcd6[bin]])
 
