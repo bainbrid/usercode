79147fd581994cbefc8f5ad3729fec73a60c1167
bainbrid <>
2010-08-24 15:17:14 +0000

    now based on reco, not gen

--- SUSY/python/skim_cfg.py
+++ SUSY/python/skim_cfg.py
@@ -8,12 +8,20 @@ from golden_cff import *
 # -----------------------------------------------------------------------------
 # Selection and cut flow
 
-conf = deepcopy(conf_ic5_gen)
-conf.Common.Jets.PtCut=10.0
-conf.Common.Jets.EtaCut=5.0
+conf = deepcopy(conf_ak5_calo)
+
+conf.XCleaning.Verbose=False
+conf.XCleaning.MuonJet=False
+conf.XCleaning.ElectronJet=False
+conf.XCleaning.PhotonJet=False
+conf.XCleaning.ResolveConflicts=False
+
+conf.Common.ApplyXCleaning=False
+conf.Common.Jets.EtaCut=3.0 
+conf.Common.Jets.PtCut=20.0
 
 numComJets = OP_NumComJets(">=",2)
-alphaT = OP_CommonAlphaTCut(0.50)
+alphaT = RobOps(PSet(Algo="HadronicAlphaT",Cut=0.5).ps())
 
 def addCutFlow(a) :
     a+=numComJets
@@ -31,15 +39,24 @@ skim_ps = PSet(
 # -----------------------------------------------------------------------------
 # Samples
 
-from samples_cff import *
+qcd=PSet(
+    Name="QCDPythia6",
+    File=["/vols/cms02/bm409/QCD_Pythia6_*GeV.root"],
+    Weights = PSet(
+    CrossSection = [ 8.762e+08, 6.041e+07, 9.238e+05, 2.547e+04, 1.256e+03, 8.798e+01, 2.186e+00, 1.122e-02 ],
+    Events       = [ 6246300,   5228992,   3203440,   3132800,   3274202,   2143390,   2143921,   1184123   ],
+    PtBin        = [ 15.,       30.,       80.,       170.,      300.,      470.,      800.,      1400.     ],
+    ),
+    Format=("ICF",2),
+    )
+#File=["results/prescale100/Skim_QCDPythia6_Pt*.root"]
 
 # -----------------------------------------------------------------------------
 # Definition of analyses
 
-for bin in range(0,len(qcd6)) :
-    anal = Analysis(qcd6[bin].Name)
-    addCutFlow(anal)
-    skim = SkimOp( skim_ps.ps() )
-    anal += skim
-    anal.Run("../results",conf,[qcd6[bin]])
+anal = Analysis("QCDPythia6")
+addCutFlow(anal)
+skim = SkimOp( skim_ps.ps() )
+anal += skim
+anal.Run("../results",conf,[qcd])
 
